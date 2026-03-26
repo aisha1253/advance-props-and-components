f20c5dedfd9a879a59b0300832b269df0f7e8d2c
aisha1253 <aishaasghar01022005@gmail.com>
2026-03-26 21:42:15 +0500

    context api create context and provider component

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import { createContext, useState } from "react"
+
+const ThemeContext = createContext();
+const AuthContext = createContext();
+
+
+export function ThemeProvider  ({children}){
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = ()=>{
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'))
+  }
+}
 
 function ThemeToggler() {
   return (
